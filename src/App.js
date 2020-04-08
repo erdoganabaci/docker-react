f45d21b9ec62d8e97ef02c566fec2a840b33206c
osboxes.org <osboxes@osboxes.org>
2020-04-08 10:19:02 -0400

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Erdogan 
+          Create by Erdogan 
         </p>
         <a
           className="App-link"
